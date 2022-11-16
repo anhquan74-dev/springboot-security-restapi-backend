d4fac7b0a0260c10bddbc3c683731d7673e71328
anhquan74-dev <anhquan74.dev@gmail.com>
2022-11-16 11:58:34 +0700

    first commit

---
+++ src/main/java/edu/dac/springbootsecurityrestapibackend/SpringbootSecurityRestapiBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.dac.springbootsecurityrestapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityRestapiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityRestapiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dac/springbootsecurityrestapibackend/SpringbootSecurityRestapiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dac.springbootsecurityrestapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityRestapiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
